d3bd21a3cc65fa4f42eec97b812ebe24e6907af4
Saurabh <gohainsaurabh@gmail.com>
2022-12-12 22:24:20 +0530

    Working on appjsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import Header from './components/Header/Header';
 import './App.css';
+import {BrowserRouter as Router} from "react-router-dom"
+import WebFont from "webfontloader"
+import React from 'react';
 
 function App() {
+  React.useEffect(()=>{
+    WebFont.load({
+      google: {
+        families:["Roboto","Droid Sans","Chilanka"]
+      },
+    });
+  }, []);
   return (
-    <div>
+    <Router>
       <Header/>
-    </div>
+    </Router>
   );
 }
 
