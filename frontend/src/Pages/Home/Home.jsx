486148fc5c21bb1771d68568cb646ca31097ee21
Hirak7024 <hirakjyotidas2808000@gmail.com>
2022-12-13 02:04:35 +0530

    Card Component finished

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Card from '../../components/Card/Card'
+import '../../components/Card/Card.css'
 
 const Home = () => {
   return (
+    <>
     <div>Home</div>
+    <div>
+      <Card />
+    </div>
+    </>
   )
 }
 
---
+++ frontend/src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+export default function Card() {
+    const handleClick = event => {
+        console.log("Card Clicked");
+      };
+    return (
+        <>
+                <div className="card" onClick={handleClick}>
+                    <h4>Prepare By Topics</h4>
+                    <h2>Java</h2>
+                    <button className="btn">Continue Preparation</button>
+                </div>
+        </>
+    )
+}
