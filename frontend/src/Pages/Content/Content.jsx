1171d8724d6251f407e4aa765d181a43b800baf6
Saurabh <gohainsaurabh@gmail.com>
2022-12-13 00:07:11 +0530

    Added Content page

--- frontend/src/Pages/Content/Content.jsx
+++ frontend/src/Pages/Content/Content.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import Description from '../../components/Description/Description'
+import IDE from '../../components/IDE/IDE'
 const Content = () => {
-  return (
-    <div>Content</div>
-  )
+    return (
+        <div>
+        <div className="MainContainer">
+          {/* <Header /> */}
+          <div className="Container">
+            <Description />
+            <IDE />
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default Content
