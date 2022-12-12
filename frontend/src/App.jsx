4c39ca157f588cd4fc27453f5f9f4f4aa7e9ef75
Saurabh <gohainsaurabh@gmail.com>
2022-12-12 23:30:37 +0530

    Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import Header from './components/Header/Header';
 import './App.css';
-import {BrowserRouter as Router} from "react-router-dom"
+import {BrowserRouter as Router,Routes, Route} from "react-router-dom"
 import WebFont from "webfontloader"
 import React from 'react';
-
+import Home from './Pages/Home/Home';
+import Content from './Pages/Content/Content';
 function App() {
   React.useEffect(()=>{
     WebFont.load({
@@ -15,6 +16,11 @@ function App() {
   return (
     <Router>
       <Header/>
+      <Routes>
+      <Route exact path="/" element={ <Home/>}/>
+      <Route exact path="/content" element={ <Content/>}/>
+      <Route path="*" element={<div>Page not found 404</div>}/>
+      </Routes>
     </Router>
   );
 }
