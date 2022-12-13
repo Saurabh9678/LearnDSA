e697e986350d7bedf28af501613ecf196b497ed8
Hirak7024 <hirakjyotidas2808000@gmail.com>
2022-12-13 20:01:41 +0530

    Finished Register Page

---
+++ frontend/src/components/Register/Register1.jsx
@@ -0,0 +1,20 @@
+export default function Register1() {
+    return (
+        <div className="RegisterContainer">
+            <div className="RegisterForm">
+                <div className="Head">
+                    <h2>Sign Up</h2>
+                </div>
+                <div className="formName">
+                    <form action="/" className="form">
+                        <input type="text" className="inputform" placeholder="Enter Your Name" />
+                        <input type="email" className="inputform" placeholder="Enter Your Email" />
+                        <input type="password" className="inputform" placeholder="Enter Your Password" />
+                        <input type="submit" className="submit" value="Create an Account" />
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    )
+}
