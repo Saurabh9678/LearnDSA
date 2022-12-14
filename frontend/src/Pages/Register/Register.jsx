62e77cce169bab8690e03a2f2d9d6c57469da800
Hirak7024 <hirakjyotidas2808000@gmail.com>
2022-12-14 20:39:22 +0530

    login and register page new design

--- frontend/src/Pages/Register/Register.jsx
+++ frontend/src/Pages/Register/Register.jsx
@@ -4,24 +4,27 @@ import "./Register.css"
 const Register = () => {
   return (
     <>
-       <div className="RegisterContainer">
-            <div className="RegisterForm">
-                <div className="Head">
-                    <h2>Sign Up</h2>
-                </div>
-                <div className="formName">
-                    <form action="/" className="form" method='post'>
-                        <input type="text" className="inputForm" placeholder="Enter your Name" />
-                        <input type="email" className="inputForm" placeholder="Enter your Email" />
-                        <input type="password" className="inputForm" placeholder="Enter your Password" />
-                        <button className='registerBtn'>Create an Account</button>
-                    </form>
-
-                </div>
-            </div>
+    <div className="signupBox">
+      <h2>Sign Up</h2>
+      <form >
+        <div className="userFormBox">
+          <input type="text" name="" required="" />
+          <label>Name</label>
         </div>
-    
-    </>
+        <div className='userFormBox'>
+          <input type="email" name="" required="" />
+          <label>Email</label>
+        </div>
+        <div className="userFormBox">
+          <input type="password" name="" required="" />
+          <label>Password</label>
+        </div>
+        <div className="buttonFormRegister">
+          <a id='signupBtn' href="#">Submit</a>
+        </div>
+      </form>
+    </div>
+  </>
   )
 }
 
